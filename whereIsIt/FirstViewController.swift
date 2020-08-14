98f644d6c5d7bea84188374daac900790630df4e
Aika Okemoto <aika2004oke@gmail.com>
2020-08-14 17:43:50 +0900

    Initial Commit

---
+++ whereIsIt/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  whereIsIt
+//
+//  Created by 桶本あいか on 2020/08/14.
+//  Copyright © 2020 Okemoto.Okke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ whereIsIt/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  whereIsIt
+//
+//  Created by 桶本あいか on 2020/08/14.
+//  Copyright © 2020 Okemoto.Okke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
